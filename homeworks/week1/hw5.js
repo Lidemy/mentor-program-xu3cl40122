ad69b624eb609bf0e0af9132a5039b9687877efe
xu3cl40122 <xu3cl40122@gmail.com>
2018-02-03 12:14:42 +0800

    'fix_hw5'

--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -3,12 +3,61 @@ export const add = (a, b) => {
   return parseInt(a, 10) + parseInt(b, 10);
 }
 */
-
-export const bigAdd = (a,b) => {
+/*
+const add = (a,b) => {
+  console.log("123");
   var ans = "";
   for(var i = 0; i < a.length; i++ ){
     var n = parseInt(a[i]) + parseInt(b[i]);
+    console.log("a",a[i]);
+    console.log("b",b[i]);
+    console.log("n",n);
     ans += n.toString();
   }
+  console.log(ans);
   return ans;
 }
+add(123,456)
+*/
+export const add = (a,b) => {
+  var ans = "";
+  var plusOne = false;
+  a = a.split('');
+  b = b.split('');
+  var tmp
+  if(a.length < b.length){//確保a較長
+    tmp = a; 
+    a = b; 
+    b = tmp;
+  }
+  for (var i = 0; i<=(a.length - b.length); i++){//補足b的長度
+    if(a.length == b.length){
+      break;
+    }
+    b.unshift(0)
+  }
+  //console.log("a,b",a,b)
+  for(var i = a.length-1; i >= 0; i--){
+    //console.log("a",parseInt(a[i]))
+    //console.log('b',parseInt(b[i]))
+    var n = parseInt(a[i]) + parseInt(b[i]);
+    //console.log(n)
+    if(plusOne){//加上前一個的進位
+      n += 1;
+      plusOne = false;
+    }
+    if (n >=10){//進位的話取尾數
+      plusOne = true;
+      n = n%10;
+    }
+    ans = ""+n+ans;
+    //console.log('n',n)
+    //console.log(ans);
+  }
+if(plusOne){//最後的進位
+  ans = "1"+ans;
+}
+console.log(ans);
+return ans
+}
+add("12312383813881381381","129018313819319831");
--- homeworks/week1/hw5.test.js
+++ homeworks/week1/hw5.test.js
@@ -1,7 +1,10 @@
-import {bigAdd} from './hw5'
+import {add} from './hw5'
 
 describe("hw5", () => {
   it("should return correct answer when a=10 and b=10", () => {
-    expect(bigAdd('10', '10')).toBe('20')
+    expect(add('10', '10')).toBe('20')
+  })
+  it("12312383813881381381",() => {
+    expect(add("12312383813881381381","129018313819319831")).toBe("12441402127700701212")
   })
 })
