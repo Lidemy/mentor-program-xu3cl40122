cea9abd2c3795d55edeb76c226050d3275bf9f4a
xu3cl40122 <xu3cl40122@gmail.com>
2018-02-25 13:39:07 +0800

    0225

--- homeworks/week4/hw1/caculator.js
+++ homeworks/week4/hw1/caculator.js
@@ -2,19 +2,24 @@ const dq = (t) =>{
 	return document.querySelector(t)
 } 
 var disNum = 0
+var temp = 0
+var op =''
 const numBut = (n) => {
 	if(document.querySelector('#n').value == 0){
 		document.querySelector('#n').value = n.toString()
-		//disNum = document.querySelector('#n').value
+		disNum = document.querySelector('#n').value
+		console.log(disNum)
 	}
 	else{
 		document.querySelector('#n').value += n.toString()
-		//disNum = document.querySelector('#n').value 
+		disNum = document.querySelector('#n').value
+		console.log(disNum)
 	}
 }
+//--- loaded ---
 document.addEventListener('DOMContentLoaded',() =>{
-	//數字鍵
-
+	alert('本計算目前僅支援基本加減功能，ex: 3 + 5 = 8 ')
+	//數字鍵onclick
 	var btnNum = document.querySelectorAll('.btnNum')
 	for(var i = 0; i < btnNum.length; i++){
 		btnNum[i].addEventListener('click',(e) =>{
@@ -25,19 +30,31 @@ document.addEventListener('DOMContentLoaded',() =>{
 	//reset
 	dq('.c').addEventListener('click',() =>{
 		dq('#n').value = '0'
+		disNum = 0
 	})
 
-	//運算
-	var temp = 0
-	var calculating = false
+	//---運算---
+	
 	dq('.plus').addEventListener('click',() =>{
 		temp = parseFloat(dq('#n').value)
-		console.log(temp)
 		dq('#n').value = 0
+		op = '+'
+	})
+	dq('.minus').addEventListener('click',() =>{
+		temp = parseFloat(dq('#n').value)
+		dq('#n').value = 0
+		op = '-'
 	})
 	dq('.equal').addEventListener('click', () =>{
-		dq('#n').value = temp
-		temp = 0
+		switch(op){
+			case '+':
+				dq('#n').value = temp + parseFloat(dq('#n').value)
+				disNum = dq('#n').value
+				break;
+			case '-':
+				dq('#n').value = temp - parseFloat(dq('#n').value)
+				disNum = dq('#n').value
+				break;
+		}
 	})
-
 })
