5709969be43d9e905608ac24d13d8b7ce3504677
xu3cl40122 <xu3cl40122@gmail.com>
2018-03-26 11:22:31 +0800

    fd_rwd

--- homeworks/week5/hw2/board.php
+++ homeworks/week5/hw2/board.php
@@ -7,6 +7,9 @@
 		<link rel="stylesheet" type="text/css" href="normalize.css">
 		<link rel="stylesheet" type="text/css" href="board.css">
 		<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="board.js"></script>
 		<title>留言板</title>
 	</head>
@@ -27,14 +30,16 @@
 	?>
 	<body>
 		<!-- navBar -->
-		<div class="navbar">
+		
+		<div class="d_navbar">
 			<div class="logo"><h1>iDearcard</h1></div>
 			<div class="navList">
-				<div class="navButton logout">登出</div>
+				<div class="navButton logout ">登出</div>
 				<div class="navButton navBar_username"><?php echo htmlspecialchars($us_row['nickname'],ENT_QUOTES, 'utf-8');  ?></div>
 			</div>
 		</div>
-		<div class="container">
+
+		<div class="dcontainer">
 			<!-- 留言表單 -->
 			<div class="mainComment_container">
 				<form class="mainComment_form">
