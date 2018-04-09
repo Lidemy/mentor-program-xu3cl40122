241018f0ba4cfb1cd6f0c7a3c74c92c7bdead43b
xu3cl40122 <xu3cl40122@gmail.com>
2018-04-09 22:41:36 +0800

    update_todo

--- homeworks/week6/hw2/reRender.js
+++ homeworks/week6/hw2/reRender.js
@@ -1,3 +1,4 @@
+var list = []
 const addTask = (value)=>{
 	let t = `<div class="thing_container">
 				<div class="circle"></div>
@@ -6,21 +7,25 @@ const addTask = (value)=>{
 			</div>` 
 	$('.thing_row').append(t)
 }
-
-
-
-$(document).ready(() => {
-    // --- 增加todo ---
-    $('#add_task').click((e) => {
-        e.preventDefault()
-        if ($('#add_value').val() != '') {
-            addTask($('#add_value').val())
-            $('#add_value').val(() => {
+function render(){
+	$('.thing_row').empty()
+	for(let i = 0; i< list.length; i++){
+		addTask(list[i])
+	}
+}
+$(document).ready(()=>{
+	//---- add todo ---
+	$('#add_task').click((e)=>{
+		e.preventDefault()
+		if ($('#add_value').val() != ''){
+			list.push($('#add_value').val())
+			$('#add_value').val(() => {
                 return ''
             })
-        }
-    })
-
+			render()
+		}
+	})
+	// --- toggle form ---
     $('.add_button').click((e)=>{
     	$('.add_task_form').toggleClass('display-block')
     })
@@ -35,8 +40,6 @@ $(document).ready(() => {
 			$(e.target).parent().toggleClass('thing_container-done')
 		}
 	})
-
-
 
 
 
