74f4721699368fb5d5c42eedf1d25e79faa8b4df
xu3cl40122 <xu3cl40122@gmail.com>
2018-05-05 21:58:53 +0800

    BF_ajax

--- homeworks/week12/hw2/src/index.js
+++ homeworks/week12/hw2/src/index.js
@@ -1,6 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class Home extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            posts: []
+        }
+    }
+    render(){
+        const {posts} = this.state
+        return(
+            <div>
+                <h2>Blog posts</h2>
+                <ul className="list-group">
+                    {posts.map(post =>{
+                        return(
+                            <li className="list-group-item">{post.title}</li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+class About extends React.Component{
+    render(){
+        return(
+            <form>
+                <h2>Contact us</h2>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        )
+    }
+}
+
 class Navbar extends React.Component {
     constructor(props) {
         super(props)
@@ -26,17 +74,18 @@ class Navbar extends React.Component {
                     <a className="navbar-brand" href="#">BLOG2</a>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item active">
+                            <li className={"nav-item " + (tab ==='home'? 'active' : '')}>
                                 <a className="nav-link" name='home' onClick={this.handleTab}  >Home <span className="sr-only">(current)</span></a>
                             </li>
-                            <li className="nav-item">
+                            <li className={"nav-item " + (tab === 'about' && 'active')}>
                                 <a className="nav-link" name='about' onClick={this.handleTab} >About</a>
                             </li>
                         </ul>
                     </div>
                 </nav>
                 <div className='container'>
-                    {tab}
+                    {tab === 'home' && <Home />}
+                    {tab === 'about' && <About />}
                 </div>
             </div>
         )
